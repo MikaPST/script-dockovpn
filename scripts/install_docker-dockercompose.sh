272d78ff7058e3ff172c13e4b6578e55a25539f2
Michaël <56961056+MikaPST@users.noreply.github.com>
2023-02-25 12:04:56 +0100

    Create install_docker-dockercompose.sh

---
+++ scripts/install_docker-dockercompose.sh
@@ -0,0 +1,6 @@
+    git clone https://github.com/MikaPST/script-docker-dockercompose.git \
+    && cd script-docker-dockercompose/ \
+    && sudo chmod +x start.sh \
+    && sudo ./start.sh \
+    && cd .. \
+    && sudo rm -rf script-docker-dockercompose
