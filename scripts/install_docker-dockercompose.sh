3267500fbaa43b87f5053d02f85bafaf2087d6f0
Michaël <56961056+MikaPST@users.noreply.github.com>
2023-02-25 12:07:00 +0100

    Update install_docker-dockercompose.sh

--- scripts/install_docker-dockercompose.sh
+++ scripts/install_docker-dockercompose.sh
@@ -1,6 +1,12 @@
-    git clone https://github.com/MikaPST/script-docker-dockercompose.git \
-    && cd script-docker-dockercompose/ \
-    && sudo chmod +x start.sh \
-    && sudo ./start.sh \
-    && cd .. \
-    && sudo rm -rf script-docker-dockercompose
+#!/bin/bash
+
+###### Script Install Docker and Docker-Compose (Debian/Debian-based) ######
+# Projet Github by MikaPST
+# https://github.com/MikaPST/script-docker-dockercompose
+
+git clone https://github.com/MikaPST/script-docker-dockercompose.git \
+&& cd script-docker-dockercompose/ \
+&& sudo chmod +x start.sh \
+&& sudo ./start.sh \
+&& cd .. \
+&& sudo rm -rf script-docker-dockercompose
