6b021f5c6e1bb10a5ae1b461c991f40c29f211ff
Michaël <56961056+MikaPST@users.noreply.github.com>
2023-03-03 16:46:03 +0100

    Update regenerate_client.sh

--- scripts/regenerate_client.sh
+++ scripts/regenerate_client.sh
@@ -5,7 +5,7 @@
 # https://github.com/dockovpn/dockovpn
 
 # Check that the client.ovpn file was downloaded successfully
-if ! find ~/ -name "client.ovpn" -print -quit | grep -q "."; then
+if ! find openvpn_conf/ -name "client.ovpn" -print -quit | grep -q "."; then
     echo -e "\033[31mThe client.ovpn file was not downloaded successfully from the OpenVPN container. Please check Docker logs for more information.\033[0m"
     exit 1
 fi
